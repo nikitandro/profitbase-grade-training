af43038b4f0890919c642fd3696e7af10aa78836
nikitandro <nik.vish.93@mail.ru>
2024-08-24 10:57:37 +0500

    animations

--- src/app/app/app.component.ts
+++ src/app/app/app.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { classRegistryProvider, manageableClassRegistryProvider } from '@app/app/di/class-registry';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
   providers: [
--- src/app/pages/animations-page/animations-page.component.ts
+++ src/app/pages/animations-page/animations-page.component.ts
@@ -1,5 +1,6 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
 import { MessageModule } from 'primeng/message';
+import { animate, state, style, transition, trigger, } from '@angular/animations';
 
 @Component({
   selector: 'app-animations-page',
@@ -10,7 +11,44 @@ import { MessageModule } from 'primeng/message';
   imports: [
     MessageModule,
   ],
+  animations: [
+    trigger('rotate', [
+      state('initial', style({ transform: 'rotate(0deg)' })),
+      state('rotated', style({ transform: 'rotate(180deg)' })),
+      transition(
+        '* => *',
+        animate('0.3s ease')
+      )
+    ]),
+    trigger('bounce', [
+      state('initial', style({ scale: '1' })),
+      state('bounced', style({ scale: '1.2' })),
+      transition(
+        '* => *',
+        animate('0.2s ease')
+      )
+    ]),
+    trigger('hide', [
+      state('initial', style({})),
+      state('hidden', style({ scale: 0 })),
+      transition('* => *', animate('0.3s ease')),
+    ])
+  ],
 })
 export class AnimationsPageComponent {
+  protected readonly block1State = signal<string>('initial');
+  protected readonly block2State = signal<string>('initial');
+  protected readonly block3State = signal<string>('initial');
 
+  protected rotateBlock1(): void {
+    this.block1State.set(this.block1State() === 'initial' ? 'rotated' : 'initial');
+  }
+
+  protected bounceBlock2(): void {
+    this.block2State.set(this.block2State() === 'initial' ? 'bounced' : 'initial');
+  }
+
+  protected hideBlock3(): void {
+    this.block3State.set(this.block3State() === 'initial' ? 'hidden' : 'initial');
+  }
 }
